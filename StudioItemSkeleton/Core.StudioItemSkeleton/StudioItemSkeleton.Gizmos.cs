9104177d9aad71a680b78451d1104d69d3e69af2
Njaecha <andre.grabowich@gmail.com>
2024-10-28 03:22:35 +0100

    v1.0.0

---
+++ StudioItemSkeleton/Core.StudioItemSkeleton/StudioItemSkeleton.Gizmos.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using KKAPI.Studio;
+using Studio;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace StudioItemSkeleton
+{
+    internal class StudioItemSkeletonGizmos: MonoBehaviour
+    {
+        private static readonly int SrcBlend = Shader.PropertyToID("_SrcBlend");
+        private static readonly int DstBlend = Shader.PropertyToID("_DstBlend");
+        private static readonly int Cull = Shader.PropertyToID("_Cull");
+        private static readonly int ZWrite = Shader.PropertyToID("_ZWrite");
+        private static readonly int ZTest = Shader.PropertyToID("_ZTest");
+        private Material _gizmoMaterial;
+
+        private void Awake()
+        {
+            Shader shader = Shader.Find("Hidden/Internal-Colored");
+            _gizmoMaterial = new Material(shader)
+            {
+                hideFlags = HideFlags.HideAndDontSave
+            };
+            
+            // Turn on alpha blending
+            _gizmoMaterial.SetInt(SrcBlend, (int)BlendMode.SrcAlpha);
+            _gizmoMaterial.SetInt(DstBlend, (int)BlendMode.OneMinusSrcAlpha);
+            // Always draw
+            _gizmoMaterial.SetInt(Cull, (int)CullMode.Off);
+            _gizmoMaterial.SetInt(ZWrite, 0);
+            _gizmoMaterial.SetInt(ZTest, (int)CompareFunction.Always);
+        }
+
+        private void OnPostRender()
+        {
+            _gizmoMaterial.SetPass(0);
+            GL.PushMatrix();
+            GL.MultMatrix(Matrix4x4.identity);
+            
+            StudioAPI.GetSelectedObjects().ToList().ForEach(oci =>
+            {
+                if (!(oci is OCIItem item) || !item.itemFKCtrl.isActiveAndEnabled) return;
+                GL.Begin(GL.LINES);
+                GL.Color(StudioItemSkeleton.GizmoColor.Value);
+                List<Transform> goBones = item.listBones.Select(bone => bone.guideObject.transformTarget).ToList();
+                item.listBones.ForEach(bone =>
+                {
+                    Transform parent = bone.guideObject.transformTarget.parent;
+                    while (!goBones.Contains(parent))
+                    {
+                        parent = parent.parent;
+                        if (parent == item.objectItem.transform) return;
+                    }
+                    GL.Vertex(bone.guideObject.transformTarget.position);
+                    GL.Vertex(parent.position);
+                });
+                GL.End();
+            });
+            
+            GL.PopMatrix();
+        }
+    }
+}
--- StudioItemSkeleton/Core.StudioItemSkeleton/StudioItemSkeleton.cs
+++ StudioItemSkeleton/Core.StudioItemSkeleton/StudioItemSkeleton.cs
@@ -1,9 +1,16 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using BepInEx.Logging;
+using Screencap;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace StudioItemSkeleton
 {
     [BepInPlugin(GUID, PluginName, Version)]
+    [BepInProcess(KKAPI.KoikatuAPI.StudioProcessName)]
+    [BepInDependency(KKAPI.KoikatuAPI.GUID, KKAPI.KoikatuAPI.VersionConst)]
+    [BepInDependency(ScreenshotManager.GUID, ScreenshotManager.Version)]
     public class StudioItemSkeleton : BaseUnityPlugin
     {
         public const string PluginName = "StudioItemSkeleton";
@@ -11,10 +18,36 @@ namespace StudioItemSkeleton
         public const string Version = "1.0.0";
         
         internal new static ManualLogSource Logger;
+        internal static StudioItemSkeletonGizmos Gizmos;
+
+        internal static ConfigEntry<Color> GizmoColor;
         
         void Awake()
         {
             Logger = base.Logger;
+            GizmoColor = Config.Bind("General", "Line Color", Color.white, "Color of the item skeleton");
+        }
+
+        void Start()
+        {
+            ScreenshotManager.OnPreCapture += ScreenshotManager_OnPreCapture;
+            ScreenshotManager.OnPostCapture += ScreenshotManager_OnPostCapture;
+            SceneManager.sceneLoaded += (s, lsm) => CreateStudioButton(s.name);
+        }
+
+        private static void CreateStudioButton(string arg0Name)
+        {
+            Gizmos = Camera.main.GetOrAddComponent<StudioItemSkeletonGizmos>();
+        }
+
+        private static void ScreenshotManager_OnPostCapture()
+        {
+            Gizmos.enabled = true;
+        }
+
+        private static void ScreenshotManager_OnPreCapture()
+        {
+            Gizmos.enabled = false;
         }
     }
 }
