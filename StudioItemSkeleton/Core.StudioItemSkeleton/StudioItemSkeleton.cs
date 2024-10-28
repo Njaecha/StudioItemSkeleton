34ddad01001f221dcf7814737f77bd3e0235958d
Njaecha <andre.grabowich@gmail.com>
2024-10-28 01:43:48 +0100

    Add files

---
+++ StudioItemSkeleton/Core.StudioItemSkeleton/StudioItemSkeleton.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace StudioItemSkeleton
+{
+    [BepInPlugin(GUID, PluginName, Version)]
+    public class StudioItemSkeleton : BaseUnityPlugin
+    {
+        public const string PluginName = "StudioItemSkeleton";
+        public const string GUID = "org.njaecha.plugins.studioitemskeleton";
+        public const string Version = "1.0.0";
+        
+        internal new static ManualLogSource Logger;
+        
+        void Awake()
+        {
+            Logger = base.Logger;
+        }
+    }
+}
+
